1f5184ff66994752a272db95e82ad5328683395f
caron <matthieu.caron@laposte.net>
2016-12-06 13:19:24 +0100

    up

--- tp2/length.py
+++ tp2/length.py
@@ -24,4 +24,40 @@ plt.hist(taille_f,bins=nbins,color="pink")
 plt.hist(X1,bins=nbins,color='green')
 
 def loi_marginale(lesTailles) :
-    loi_marginale = [0]*(taille_max - taille_min)
+    loi_marginale = [0]*taille_max
+    for taille in lesTailles :
+        loi_marginale[int(taille)] += 1
+    return loi_marginale
+
+def mode(marginale) :
+    max_value = 0
+    cm_save = 0
+    for cm in range(len(marginale)) :
+        if marginale[cm] > max_value :
+            max_value = marginale[cm]
+            cm_save = cm
+    return cm_save
+    
+def esperance(marginale,nbSample) :
+    esp = 0
+    for cm in range(len(marginale)) :
+        esp += cm * (marginale[cm]/nbSample)
+    return esp
+        
+def mediane(marginale,nbSample) :
+    nbVues = 0
+    for cm in range(len(marginale)) :
+        nbVues += marginale[cm]
+        if nbVues >= nbSample/2 :
+            return cm
+
+marginale_hommes = loi_marginale(taille_h)
+marginale_femmes = loi_marginale(taille_f)  
+marginale_x1 = loi_marginale(X1)
+taille_frequente = mode(marginale_x1)
+taille_moyenne = esperance(marginale_x1,len(X1))
+taille_mediane = mediane(marginale_x1,len(X1))
+ 
+# question5
+ 
+ 
